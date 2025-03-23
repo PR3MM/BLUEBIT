b22bbd642d2c24710e11aa76319b875a8e437873
Premved Dhote <premveddhote@gmail.com>
2025-03-23 16:11:34 +0530

    Add  Speed Insights integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
+
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
